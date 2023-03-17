91e35c5bb651691fc8e2d3bba0d8db346fca8419
kingjohn360 <kingjohnokoye04@gmail.com>
2023-03-17 15:40:25 +0000

    initial

---
+++ learn_to_code.js
@@ -0,0 +1,44 @@
+// var name = "Jack";
+// var age = 23;
+
+// var message = "Hi my name is " + name + " and I am " + age + " years old";
+
+// var sum = 10 + 5;
+// var div = 9 / 3;
+// var fix = 10 % 3;
+
+// console.log(fix);
+
+// var val1 = 23;
+// var val2 = "23"; 
+
+// if(val1 === val2) {
+//     console.log("The numbers are the same!")
+// }
+// else {
+//     console.log("The numbers are not the same")
+// }
+
+
+// Javascript Array
+// var studentNames = ["Timmy", "Rose", "Arun"];
+// var naughtyList = [];
+
+// naughtyList.push(studentNames[0]);
+// naughtyList.push(studentNames[1]);
+// naughtyList.push(studentNames[2]);
+
+// var index = naughtyList.indexOf("Timmy");
+// console.log(naughtyList);
+
+// if (index > -1) {
+//     naughtyList.splice(index, 1);
+// }
+// console.log(naughtyList);
+
+// Javascript loops (for, while, do_while)
+var students = ["John", "Mary", "Steven", "Rose", "Timmy", "Arun", "Isaac"];
+
+for (let student in students) {
+    console.log(students[student]);
+}
